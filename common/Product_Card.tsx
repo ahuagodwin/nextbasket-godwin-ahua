9e8edebfbc04ba5be78bc30e1b13689516de802b
Ahua Godwin <57255108+ahuagodwin@users.noreply.github.com>
2024-01-16 09:42:19 +0100

    feat: continue checking if their is any recursive loop but none

--- common/Product_Card.tsx
+++ common/Product_Card.tsx
@@ -1,7 +1,5 @@
-"use client"
-
 import { paths } from "@/constants";
-import { Card, Img, Section, Text, Span, Anchor,  Button, FlexGrid } from "@/elements";
+import { Card, Img, Section, Text, Span, Anchor, Button, FlexGrid } from "@/elements";
 import { Images } from "@/public";
 import { appService } from "@/services";
 import { AppDispatch, RootState } from "@/store/store";
@@ -10,66 +8,66 @@ import { formatAmount } from "@/utils/helper";
 import React, { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-
 const Products: React.FC<ProductsProps> = ({ displayButton, sliceAmount }) => {
-  const userRef = useRef(false)
+  const isMountedRef = useRef(true);
 
   // fetching products from the store
-  const data  = useSelector((state: RootState) => state.products)
-  const dispatch = useDispatch<AppDispatch>()
+  const data = useSelector((state: RootState) => state.products);
+  const dispatch = useDispatch<AppDispatch>();
   const [displayedProducts, setDisplayedProducts] = React.useState<number>(sliceAmount);
 
-
- useEffect(() => {
-    if(userRef.current === false) {
+  useEffect(() => {
+    if (isMountedRef.current) {
       dispatch(appService.fetchProducts());
     }
     return () => {
-      userRef.current === true
-    }
- },[])
+      isMountedRef.current = false;
+    };
+  }, [dispatch]);
 
- const allProducts = data?.products?.products
+  const allProducts = data?.products?.products || [];
 
- // a function to handle loading more products
- const handleLoadMore = () => {
-  const nextDisplayedProducts = displayedProducts + 5;
-  setDisplayedProducts(nextDisplayedProducts);
-};
+  // a function to handle loading more products
+  const handleLoadMore = () => {
+    const nextDisplayedProducts = displayedProducts + 5;
+    setDisplayedProducts(nextDisplayedProducts);
+  };
 
-const shouldDisplayLoadMoreButton = allProducts && displayedProducts < allProducts.length;
-
+  const shouldDisplayLoadMoreButton = allProducts.length > displayedProducts;
 
   return (
     <>
-     <FlexGrid isGrid={true} gridType="grid5" isBorder={false}>
-      {Array.isArray(allProducts) && allProducts?.slice(0, displayedProducts)?.map((item) => (
-        <Card key={item?.id} className="product-card">
-          <Img src={item?.thumbnail ?? Images.Product1} alt="" width={100} height={100}/>
-          <Section className="product-content">
-            <Anchor as={`${paths.store}/${item?.category}/${item?.id}/${encodeURIComponent(item?.title)}` }><Text className="title">{item?.title?.slice(0, 15) + "..." ?? "Graphic Design"}</Text></Anchor>
-            <Text className="desc">
-              {item?.description?.slice(0, 18) + "..." ?? "English Department"}
-            </Text>
-            <Text className="price">
-              <Span>{formatAmount(item?.discountPercentage ?? "0.00")}</Span>
-              <Span>{formatAmount(item?.price ?? "0.00")}</Span>
-            </Text>
-          </Section>
-        </Card>
-      ))}
-</FlexGrid>
+      <FlexGrid isGrid={true} gridType="grid5" isBorder={false}>
+        {Array.isArray(allProducts) &&
+          allProducts.slice(0, displayedProducts).map((item) => (
+            <Card key={item?.id} className="product-card">
+              <Img src={item?.thumbnail ?? Images.Product1} alt="" width={100} height={100} />
+              <Section className="product-content">
+                <Anchor
+                  as={`${paths.store}/${item?.category}/${item?.id}/${encodeURIComponent(item?.title)}`}
+                >
+                  <Text className="title">{item?.title?.slice(0, 15) + "..."}</Text>
+                </Anchor>
+                <Text className="desc">{item?.description?.slice(0, 18) + "..." }</Text>
+                <Text className="price">
+                  <Span>{formatAmount(item?.discountPercentage ?? "0.00")}</Span>
+                  <Span>{formatAmount(item?.price ?? "0.00")}</Span>
+                </Text>
+              </Section>
+            </Card>
+          ))}
+      </FlexGrid>
 
-{/* rendering of the "LOAD MORE PRODUCT" button based on the condition*/}
+      {/* rendering of the "LOAD MORE PRODUCT" button based on the condition*/}
       {displayButton && shouldDisplayLoadMoreButton && (
         <Section className="flex justify-center items-center mx-auto mt-[50px]">
-        <Button
-          title="LOAD MORE PRODUCT"
-          stylestype="primary"
-          size="medium"
-          handleClick={handleLoadMore}
-        />
-      </Section>
+          <Button
+            title="LOAD MORE PRODUCT"
+            stylestype="primary"
+            size="medium"
+            handleClick={handleLoadMore}
+          />
+        </Section>
       )}
     </>
   );
